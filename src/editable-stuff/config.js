9c382397c78b39a67f4481b9a911b394ed210af2
Soonhyuck Hong <hong.soonhyuck@gmail.com>
2022-10-15 16:53:27 -0700

    adding my info

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -15,14 +15,10 @@ const mainBody = {
       image: "fa-github",
       url: "https://github.com/hongsoonhyuck",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com//",
-    },
+    // {
+    //   image: "fa-instagram",
+    //   url: "https://www.instagram.com//",
+    // },
     {
       image: "fa-linkedin",
       url: "https://www.linkedin.com/in/soonhyuckhong/",
@@ -47,11 +43,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/soonhyuckhong.png"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "My name is Soonhyuck Hong.",
+  resume: require("../editable-stuff/resume.pdf"),
 };
 
 // PROJECTS SECTION
@@ -62,8 +58,8 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
-  reposLength: 4,
+  gitHubUsername: "hongsoonhyucg", //i.e."johnDoe12Gh"
+  reposLength: 0,
   specificRepos: [],
 };
 
