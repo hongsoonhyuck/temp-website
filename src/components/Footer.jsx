8399a2291929ace9d18a6d6a9a80f89f57032579
Soonhyuck Hong <hong.soonhyuck@gmail.com>
2022-10-15 22:11:13 -0700

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,17 +11,17 @@ const Footer = (props) => {
         <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
         <a
           rel="noopener"
-          href="https://github.com/hashirshoaeb"
+          href="https://github.com/soonhyuckhong"
           aria-label="My GitHub"
         > <span className="badge bg-dark">
-            Hashir Shoaib
+            Soonhyuck Hong
           </span>
         </a>{" "}
         using <i className="fab fa-react" />
         <p>
           <small className="text-muted">
             Project code is open source. Feel free to fork and make your own
-            version.
+            version. Shoutout to Hashir Shoaib for the template.
           </small>
         </p>
       </Container>
