63bef7d67e2a86d5a751c89bd305e0fbcec39ed0
Soonhyuck Hong <hong.soonhyuck@gmail.com>
2022-10-18 14:50:08 -0700

    adding experience to navbar

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -9,12 +9,15 @@ const ExperienceCard = ({ data }) => {
     <Col lg="6">
       <div className="pb-5 text-center">
         <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+        {/*
         <p className="lead">
           {data.role}
           <br />
           {data.date}
         </p>
-
+        */}
+        <h4>{data.role}</h4>
+        <p className="lead">{data.date}</p>
       </div>
     </Col>
   );
